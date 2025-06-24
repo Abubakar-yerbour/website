81ae499b2d095fd23dfe1fd551e8ce5e39e5e130
Abubakar Bello <abubakarbello3914@gmail.com>
2025-06-25 00:30:12 +0100

    Update socketio_config.py

--- socketio_config.py
+++ socketio_config.py
@@ -1,3 +1,7 @@
+# socketio_config.py
 from flask_socketio import SocketIO
 
-socketio = SocketIO()
+socketio = SocketIO(async_mode='threading')
+
+def init_socketio(app):
+    socketio.init_app(app)
